51fa8a6c62956dd92569db63f7855ac5f40710a2
kelsonalmeida <kelson.victr@gmail.com>
2024-10-31 19:36:27 -0300

    app...

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ProvedorCartoesEstudo } from './src/contexts/CartoesEstudoContext';
+import ListaCartaoScreen from './src/screens/ListaCartaoScreen';
+import EdicaoCartaoScreen from './src/screens/EdicaoCartaoScreen';
+import TarefasVencimentoProximoScreen from './src/screens/TarefasVencimentoProximoScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+    return (
+        <ProvedorCartoesEstudo>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="ListaCartao">
+                    <Stack.Screen name="ListaCartao" component={ListaCartaoScreen} options={{ title: 'Cartões de Estudo' }} />
+                    <Stack.Screen name="EdicaoCartao" component={EdicaoCartaoScreen} options={{ title: 'Editar Cartão' }} />
+                    <Stack.Screen name="TarefasVencimentoProximo" component={TarefasVencimentoProximoScreen} options={{ title: 'Tarefas a Vencer' }} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </ProvedorCartoesEstudo>
+    );
+};
+
+export default App;
